a98f80922116d0fe4d40800e292dc012c8e66db7
Deb <deborahdev.io@gmail.com>
2022-10-15 11:42:07 +0200

    refactor chooseCase function

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
+#include "chooseCase.h"
 
 int main(int argc, const char * argv[]) {
 
@@ -8,57 +8,39 @@ int main(int argc, const char * argv[]) {
     int caseNumber=0;
     int *pointeurCaseNumber = &caseNumber;
 
+    int player = 1;
+
+
+
     printf("Bienvenue sur le Tic Tac Toe !\n\n");
 
-    printf("\t\t\t%c|%c|%c\n \t\t\t%c|%c|%c\n \t\t\t%c|%c|%c\n\n",board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8]);
-
-    printf("Joueur 1 (x), Choisi une case\n");
-    scanf("%d", pointeurCaseNumber);
-    //printf("je suis la case %d\n", *pointeurCaseNumber);
 
 
-    /*printf("pointeur %d \n", pointeurCaseNumber);  // return l'adresse de la variable (177450)
-    printf("&pointeur %d \n", &pointeurCaseNumber); // return l'adress du pointeur (3)
-    printf("*pointeur %d", *pointeurCaseNumber);  // return valeur de la variable*/
+        printf("\t\t\t%c|%c|%c\n \t\t\t%c|%c|%c\n \t\t\t%c|%c|%c\n\n",board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8]);
+
+        printf("Joueur ??? %d\n", player);
+        printf("Joueur 1 (x), Choisi une case\n");
+        scanf("%d", pointeurCaseNumber);
+    printf("Joueur ??? %d\n", player);
+
+        /*printf("pointeur %d \n", pointeurCaseNumber);  // return l'adresse de la variable (177450)
+        printf("&pointeur %d \n", &pointeurCaseNumber); // return l'adress du pointeur (3)
+        printf("*pointeur %d", *pointeurCaseNumber);  // return valeur de la variable*/
 
 
-    switch (caseNumber) {
-        case 1 :
-            //if (player 1) {}
-            //else {}
-            board[0] = 'x';
-            break;
-        case 2 :
-            printf("je suis la case 2");
-            break;
-        case 3 :
-            //board[0] = 0;
-            break;
-        case 4 :
-            printf("je suis la case 2");
-            break;
-        case 5 :
-            //board[0] = 0;
-            break;
-        case 6 :
-            printf("je suis la case 2");
-            break;
-        case 7 :
-            //board[0] = 0;
-            break;
-        case 8 :
-            printf("je suis la case 2");
-            break;
-        case 9 :
-            printf("je suis la case 2");
-            break;
-        default:
-            printf("je ne suis pas une case");
-            break;
-    }
+        chooseCase(&player, &caseNumber, &board);
 
-    printf("\t\t\t%c|%c|%c\n \t\t\t%c|%c|%c\n \t\t\t%c|%c|%c\n\n",board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8]);
+        printf("\t\t\t%c|%c|%c\n \t\t\t%c|%c|%c\n \t\t\t%c|%c|%c\n\n",board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8]);
+
+        printf("Joueur 2 (o), Choisi une case\n");
+        scanf("%d", pointeurCaseNumber);
+
+    chooseCase(&player, &caseNumber, &board);
+
+        printf("\t\t\t%c|%c|%c\n \t\t\t%c|%c|%c\n \t\t\t%c|%c|%c\n\n",board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8]);
+
 
     return 0;
 }
+
 
